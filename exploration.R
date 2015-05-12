5b3144fc30e594477c2478d1153260da13faa96a
Mathieu-M <m.marauri@hotmail.fr>
2015-05-12 17:33:13 +0200

    GLMM models selection

--- exploration.R
+++ exploration.R
@@ -18,7 +18,7 @@ str(cows)
 head(cows)
 summary(cows)
 cows$pcv.b <- as.numeric(cows$pcv > 20)
-cows$pcv.f <- factor(cows$pcv > 20, labels = c("Unhealthy", "Healthy"))
+cows$pcv.f <- factor(cows$pcv > 20, labels = c("Healthy", "Unhealthy"))
 cows$time.f <- paste("Time", cows$time)
 cows.com <- na.omit(cows)
 cows.com$idDose <- as.factor(paste(cows.com$id, cows.com$dose, sep = "_"))
@@ -38,10 +38,14 @@ cor(cows.w[, c("1", "2", "3")], use = "pairwise.complete.obs")
 
 library("geepack")
 
+?geese
+?geeglm
+
 # In help it is pointed in id the following
 # "Data are assumed to be sorted so that observations 
 # on a cluster are contiguous rows for all entities in the formula."
 cows.com <- cows.com %>% arrange(idDose)
+
 
 # model1 <- geeglm(pcv.b ~ time, id = idDose, data = cows.com,
 #                family = binomial, corstr = "unstructured", scale.fix = TRUE)
@@ -52,148 +56,156 @@ cows.com <- cows.com %>% arrange(idDose)
 
 model00 <- geeglm(pcv.b ~ dose, id = idDose, data = cows.com,
                  family = binomial, corstr = "exch", scale.fix = TRUE)
+model01 <- geeglm(pcv.b ~ dose + time, id = idDose, data = cows.com,
+                 family = binomial, corstr = "exch", scale.fix = TRUE)
+model02 <- geeglm(pcv.b ~ dose + nbirth, id = idDose, data = cows.com,
+                 family = binomial, corstr = "exch", scale.fix = TRUE)
+model03 <- geeglm(pcv.b ~ dose + time:dose, id = idDose, data = cows.com,
+                 family = binomial, corstr = "exch", scale.fix = TRUE)
 
 summary(model00)
-
-model01 <- update(model00, formula = ~. + time)
-model02 <- update(model00, formula = ~. + nbirth)
-
 summary(model01)
 summary(model02)
+summary(model03)
 
 # Criteria for comparision -> QIC
+# Scales changes a lot from one pseudo-likelihood from one to other
+
 library("MuMIn") # Package for GEE model selection (QIC)
-model.sel(model00, model01, model02, rank = QIC)
+model.sel(model00, model01, model02, model03, rank = QIC)
 
 anova(model00, model01)
 
 # Iteration 1
-model11 <- update(model01, formula = ~. + dose:time)
-model12 <- update(model01, formula = ~. + nbirth)
+model11 <- geeglm(pcv.b ~ dose*time, id = idDose, data = cows.com,
+                  family = binomial, corstr = "exch", scale.fix = TRUE)
+
+model12 <- geeglm(pcv.b ~ time + dose + nbirth, id = idDose, data = cows.com,
+               family = binomial, corstr = "exch", scale.fix = TRUE)
+
+model13 <- geeglm(pcv.b ~ time + dose + dose:nbirth, id = idDose, data = cows.com,
+                  family = binomial, corstr = "exch", scale.fix = TRUE)
+
+# nonstructure (weaker)
 
 summary(model11)
 summary(model12)
-
-anova(model01, model11)
-anova(model01, model12)
-
-# Some more models
-model13 <- update(model01, formula = ~. + nbirth*time)
-model14 <- update(model01, formula = ~. + nbirth*dose)
-
 summary(model13)
-summary(model14)
 
-model.sel(model01, model13, model14, rank = QIC)
-anova(model01, model13)
-anova(model01, model14)
 
-# Model 01 is the selected one
+anova(model1, model2)
+# Pseudo likelihood function
 
-# Choose best working correlation structure
+library("gee")
 
-model01ind <- update(model01, corstr = "independence")
-model01ar1 <- update(model01, corstr = "ar1")
-# model01uns <- update(model01, corstr = "unstructured") 
-# Error en `contrasts<-`(`*tmp*`, value = contr.funs[1 + isOF[nn]]) : 
-#   contrasts can be applied only to factors with 2 or more levels
+model <- geese(pcv.b ~ dose*time + nbirth, id = idDose, data = cows.com,
+               family = binomial, corstr = "exch", scale.fix = TRUE)
 
-model.sel(model01, model01ar1, model01ind, rank = QIC)
-# All models quite the similar
-
-summary(model01)
-summary(model01ar1)
-
-# The ar1 model is the one with better QIC. 
-# ar1 model smaller variance in dose coefficients
-# Exchangability smaller variance in intercept and time
-
-# Note: If cows.com was not sorted results in working correlation matrix were different!!!
-
-# Classification table
-
-pred.bin <- function(model){
-  
-  pred <- predict(model)
-  p <- exp(pred)/(1 + exp(pred))
-  round(p)
-  
-}
-
-table(pred.bin(model01ar1), cows.com$pcv.b)
-
-library("caret")
-sensitivity(as.factor(pred.bin(model01ar1)), as.factor(cows.com$pcv.b))
-specificity(as.factor(pred.bin(model01ar1)), as.factor(cows.com$pcv.b))
 
 # GLMM --------------------------------------------------------------------
 
 library("lme4")
 
+# Random effect on the intercep and the slope.
 
-modelmm1 <- glmer(pcv.b~dose*time+nbirth+(0+time|idDose),data=cows.com,family=binomial)
-summary(modelmm1)
+model.re.11 <- glmer(pcv.b~dose+(time|id/dose),data=cows.com,family=binomial,control=glmerControl(optimizer="bobyqa"))
+summary(model.re.11)
+# Correlation of -1.
 
-modelmm12 <- glmer(pcv.b~dose+time+nbirth+(0+time|id/dose),data=cows.com,family=binomial)
-summary(modelmm12)
+start11 <- unlist(getME(model.re.11,name="ST"))
+start12 <- unlist(getME(model.re.11,name="theta"))
+start13 <- unlist(getME(model.re.11,name="beta"))
+start1 <- list(c(start11,start12,start13)) 
 
-modelmm13 <- glmer(pcv.b~dose+time+(0+time|id/dose),data=cows.com,family=binomial)
-summary(modelmm13)
+model.re.12 <- glmer(pcv.b~time+(time|id/dose),data=cows.com,family=binomial,start=start1,control=glmerControl(optimizer="bobyqa"))
+summary(model.re.11)
+# The model failed to converge and corr of -1.
 
-modelmm14 <- glmer(pcv.b~time+(0+time|id/dose),data=cows.com,family=binomial)
-summary(modelmm14)
-
-modelmm15 <- glmer(pcv.b~nbirth+(0+time|id/dose),data=cows.com,family=binomial)
-summary(modelmm15)
-
-modelmm16 <- glmer(pcv.b~dose+(0+time|id/dose),data=cows.com,family=binomial)
-summary(modelmm16)
+# We will not use a random intercept.
 
 
+# Random effect only on the slope.
 
+model.res.11 <- glmer(pcv.b~dose+(0+time|id/dose),data=cows.com,family=binomial)
+summary(model.res.11)
+# AIC = 57.9
 
-modelmm2 <- glmer(pcv.b~dose*time+nbirth+(0+time|id/dose),data=cows.com,family=binomial)
-summary(modelmm2)
+start11 <- unlist(getME(model.res.11,name="ST"))
+start12 <- unlist(getME(model.res.11,name="theta"))
+start13 <- unlist(getME(model.res.11,name="beta"))
+start1 <- list(c(start11,start12,start13))
 
-modelmm21 <- glmer(pcv.b~dose+time+nbirth+(0+time|id/dose),data=cows.com,family=binomial)
-summary(modelmm21)
+model.res.12 <- glmer(pcv.b~time+(0+time|id/dose),data=cows.com,family=binomial)
+summary(model.res.12)
+# AIC = 63.7
 
-modelmm22 <- glmer(pcv.b~dose+time+(0+time|id/dose),data=cows.com,family=binomial)
-summary(modelmm22)
-# Warning
+model.res.13 <- glmer(pcv.b~nbirth+(0+time|id/dose),data=cows.com,family=binomial)
+summary(model.res.13)
+# AIC = 65.9
 
-modelmm13 <- glmer(pcv.b~time+(0+time|id/dose),data=cows.com,family=binomial)
-summary(modelmm13)
+# We use the model with dose.
+model.res.21 <- glmer(pcv.b~dose+time+(0+time|id/dose),data=cows.com,family=binomial,start=start1,control=glmerControl(optimizer="bobyqa"))
+summary(model.res.21)
+# AIC = 36.59
 
-modelmm24 <- glmer(pcv.b~nbirth+(0+time|id/dose),data=cows.com,family=binomial)
-summary(modelmm24)
+anova(model.res.11,model.res.21)
 
-modelmm25 <- glmer(pcv.b~dose+(0+time|id/dose),data=cows.com,family=binomial)
-summary(modelmm25)
+model.res.22 <- glmer(pcv.b~dose+nbirth+(0+time|id/dose),data=cows.com,family=binomial,start=start1,control=glmerControl(optimizer="bobyqa"))
+summary(model.res.22)
+# AIC = 59.9
 
-# Almost stable for dose+time
+start21 <- unlist(getME(model.res.21,name="ST"))
+start22 <- unlist(getME(model.res.21,name="theta"))
+start23 <- unlist(getME(model.res.21,name="beta"))
+start2 <- list(c(start21,start22,start23)) 
+
+# We use the model with dose+time.
+model.res.31 <- glmer(pcv.b~dose+time+nbirth+(0+time|id/dose),data=cows.com,family=binomial,start=start2,control=glmerControl(optimizer="bobyqa"))
+summary(model.res.31)
+# AIC = 38.3
+
+anova(model.res.21,model.res.31)
+# model with dose+time is better.
+
+model.res.41 <- glmer(pcv.b~dose*time+(0+time|id/dose),data=cows.com,family=binomial,start=start2,control=glmerControl(optimizer="bobyqa"))
+summary(model.res.41)
+# AIC = 39
+
+anova(model.res.21,model.res.41)
+# We keep the model with dose+time.
+
+finalModel <- model.re.21
+summary(finalModel)
+
+orfixed <- c("intercept"=exp(coef(finalModel)$id[[1]][1]),"doseM"=exp(coef(finalModel)$id[[2]][1]),
+             "doseH"=exp(coef(finalModel)$id[[3]][1]))
+
+orrandom <- exp(c(coef(finalModel)$`dose:id`[[4]],coef(finalModel)$id[[4]]))
+
+# Is it ok to have Odds Ratio either very large or very small (*10^16;*10^-37)?
+
+library("caret")
+
+pred.bin <- function(model){
+  pred <- predict(model)
+  p <- exp(pred)/(1 + exp(pred))
+  round(p)
+}
+
+prediction <- pred.bin(finalModel)
+
+confusionMatrix(data=prediction,reference=cows.com$pcv.b)
+
+# Is it ok to have a sensitivity and a specificity of 1?
+
 
 # Missingnes --------------------------------------------------------------
-
-aggregate(is.na(cows$pcv.f), list(Time=cows$time, Dose=cows$dose), sum)[,c(2,1,3)]
-
-a <- barplot(table(is.na(cows$pcv.f), paste(cows$dose, cows$time, sep="_"))[2,c(4:9, 1:3)]/nrow(cows)*100, beside=T, space=c(0,0,0,1,0,0,1,0,0), xaxt="n", ylab="%")
-abline(h=0)
-axis(1, at=a[c(2,5,8),], paste("Dose", levels(cows$dose)), tick=F, line=1.5)
-axis(1, at=a, rep(paste("T", 1:3, sep=""),3), tick=F, line=-0.5)
-## Percentatge de missings en cada dosi i temps respecte del total de dades de la mostra.
-
-fisher.test(table(is.na(cows$pcv.f),cows$dose))
-fisher.test(table(is.na(cows$pcv.f),cows$time))
-
-# oagg <- order((agg <- aggregate(is.na(cows$pcv.f), list(cows$time, cows$dose, cows$id), sum))[,"x"])
-# agg <- agg[oagg,]
-# agg <- agg[order(agg[[3]]),]
 
 # Sensitivity analysis
 # Pattern
 
 # Try to impute
+
 
 
 
