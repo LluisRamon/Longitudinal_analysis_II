3b0fd2835b63fbad09c5a8de712070e03f9931d9
Lluis Ramon <lluisramon.tgn@gmail.com>
2015-05-13 17:41:37 +0200

    Fix broken GEE and missing data part.

--- exploration.R
+++ exploration.R
@@ -18,11 +18,10 @@ str(cows)
 head(cows)
 summary(cows)
 cows$pcv.b <- as.numeric(cows$pcv > 20)
-cows$pcv.f <- factor(cows$pcv > 20, labels = c("Healthy", "Unhealthy"))
+cows$pcv.f <- factor(cows$pcv > 20, labels = c("Unhealthy", "Healthy"))
 cows$time.f <- paste("Time", cows$time)
 cows.com <- na.omit(cows)
 cows.com$idDose <- as.factor(paste(cows.com$id, cows.com$dose, sep = "_"))
-
 
 table(cows$pcv.f , cows$time.f, useNA = "ifany")
 table(cows.com$pcv.f , cows.com$time.f)
@@ -39,14 +38,10 @@ cor(cows.w[, c("1", "2", "3")], use = "pairwise.complete.obs")
 
 library("geepack")
 
-?geese
-?geeglm
-
 # In help it is pointed in id the following
 # "Data are assumed to be sorted so that observations 
 # on a cluster are contiguous rows for all entities in the formula."
 cows.com <- cows.com %>% arrange(idDose)
-
 
 # model1 <- geeglm(pcv.b ~ time, id = idDose, data = cows.com,
 #                family = binomial, corstr = "unstructured", scale.fix = TRUE)
@@ -56,52 +51,80 @@ cows.com <- cows.com %>% arrange(idDose)
 # Not converging with the unstructured
 
 model00 <- geeglm(pcv.b ~ dose, id = idDose, data = cows.com,
-                 family = binomial, corstr = "exch", scale.fix = TRUE)
-model01 <- geeglm(pcv.b ~ dose + time, id = idDose, data = cows.com,
-                 family = binomial, corstr = "exch", scale.fix = TRUE)
-model02 <- geeglm(pcv.b ~ dose + nbirth, id = idDose, data = cows.com,
-                 family = binomial, corstr = "exch", scale.fix = TRUE)
-model03 <- geeglm(pcv.b ~ dose + time:dose, id = idDose, data = cows.com,
-                 family = binomial, corstr = "exch", scale.fix = TRUE)
+                  family = binomial, corstr = "exch", scale.fix = TRUE)
 
 summary(model00)
+
+model01 <- update(model00, formula = ~. + time)
+model02 <- update(model00, formula = ~. + nbirth)
+
 summary(model01)
 summary(model02)
-summary(model03)
 
 # Criteria for comparision -> QIC
-# Scales changes a lot from one pseudo-likelihood from one to other
-
 library("MuMIn") # Package for GEE model selection (QIC)
-model.sel(model00, model01, model02, model03, rank = QIC)
+model.sel(model00, model01, model02, rank = QIC)
 
 anova(model00, model01)
 
 # Iteration 1
-model11 <- geeglm(pcv.b ~ dose*time, id = idDose, data = cows.com,
-                  family = binomial, corstr = "exch", scale.fix = TRUE)
-
-model12 <- geeglm(pcv.b ~ time + dose + nbirth, id = idDose, data = cows.com,
-               family = binomial, corstr = "exch", scale.fix = TRUE)
-
-model13 <- geeglm(pcv.b ~ time + dose + dose:nbirth, id = idDose, data = cows.com,
-                  family = binomial, corstr = "exch", scale.fix = TRUE)
-
-# nonstructure (weaker)
+model11 <- update(model01, formula = ~. + dose:time)
+model12 <- update(model01, formula = ~. + nbirth)
 
 summary(model11)
 summary(model12)
+
+anova(model01, model11)
+anova(model01, model12)
+
+# Some more models
+model13 <- update(model01, formula = ~. + nbirth*time)
+model14 <- update(model01, formula = ~. + nbirth*dose)
+
 summary(model13)
+summary(model14)
 
+model.sel(model01, model13, model14, rank = QIC)
+anova(model01, model13)
+anova(model01, model14)
 
-anova(model1, model2)
-# Pseudo likelihood function
+# Model 01 is the selected one
 
-library("gee")
+# Choose best working correlation structure
 
-model <- geese(pcv.b ~ dose*time + nbirth, id = idDose, data = cows.com,
-               family = binomial, corstr = "exch", scale.fix = TRUE)
+model01ind <- update(model01, corstr = "independence")
+model01ar1 <- update(model01, corstr = "ar1")
+# model01uns <- update(model01, corstr = "unstructured") 
+# Error en `contrasts<-`(`*tmp*`, value = contr.funs[1 + isOF[nn]]) : 
+#   contrasts can be applied only to factors with 2 or more levels
 
+model.sel(model01, model01ar1, model01ind, rank = QIC)
+# All models quite the similar
+
+summary(model01)
+summary(model01ar1)
+
+# The ar1 model is the one with better QIC. 
+# ar1 model smaller variance in dose coefficients
+# Exchangability smaller variance in intercept and time
+
+# Note: If cows.com was not sorted results in working correlation matrix were different!!!
+
+# Classification table
+
+pred.bin <- function(model){
+  
+  pred <- predict(model)
+  p <- exp(pred)/(1 + exp(pred))
+  round(p)
+  
+}
+
+table(pred.bin(model01ar1), cows.com$pcv.b)
+
+library("caret")
+sensitivity(as.factor(pred.bin(model01ar1)), as.factor(cows.com$pcv.b))
+specificity(as.factor(pred.bin(model01ar1)), as.factor(cows.com$pcv.b))
 
 # GLMM --------------------------------------------------------------------
 
@@ -184,11 +207,22 @@ confusionMatrix(data=prediction,reference=cows.com$pcv.b)
 
 # Missingnes --------------------------------------------------------------
 
+aggregate(is.na(cows$pcv.f), list(Time=cows$time, Dose=cows$dose), sum)[,c(2,1,3)]
+
+a <- barplot(table(is.na(cows$pcv.f), paste(cows$dose, cows$time, sep="_"))[2,c(4:9, 1:3)]/nrow(cows)*100, beside=T, space=c(0,0,0,1,0,0,1,0,0), xaxt="n", ylab="%")
+abline(h=0)
+axis(1, at=a[c(2,5,8),], paste("Dose", levels(cows$dose)), tick=F, line=1.5)
+axis(1, at=a, rep(paste("T", 1:3, sep=""),3), tick=F, line=-0.5)
+## Percentatge de missings en cada dosi i temps respecte del total de dades de la mostra.
+
+fisher.test(table(is.na(cows$pcv.f),cows$dose))
+fisher.test(table(is.na(cows$pcv.f),cows$time))
+
+# oagg <- order((agg <- aggregate(is.na(cows$pcv.f), list(cows$time, cows$dose, cows$id), sum))[,"x"])
+# agg <- agg[oagg,]
+# agg <- agg[order(agg[[3]]),]
+
 # Sensitivity analysis
 # Pattern
 
 # Try to impute
-
-
-
-
